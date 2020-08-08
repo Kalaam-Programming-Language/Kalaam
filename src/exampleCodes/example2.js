04f1db8a2e69e413389edeba877cc887f97f1574
Deepesh Deshmukh <kumardeepesh02@gmail.com>
2020-08-08 19:28:36 +0530

    Added tests for first three examples

---
+++ src/exampleCodes/example2.js
@@ -0,0 +1,12 @@
+const tW = (strings) => { return strings.map((s) => s.replace(/\s+/g, '\n')).join("").trim() }
+
+export const example2 = tW`YourSign='Test Sign' 
+                line="|"
+                space="  "
+                Extra="  "
+                दुहराओ x को (1,15) मे
+                {
+                Road=space+line+space+space+space+YourSign+space+space+space+line+space
+                space=space+Extra
+                दिखाए(Road)
+                }`
